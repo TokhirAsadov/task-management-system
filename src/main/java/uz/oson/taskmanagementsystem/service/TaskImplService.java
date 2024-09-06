790708af548d49db0876b6604fc0af904f6cbea6
Takhir <guvalakat1603@gmail.com>
2024-09-06 18:12:41 +0500

    getAllTasks function is override and done

--- src/main/java/uz/oson/taskmanagementsystem/service/TaskImplService.java
+++ src/main/java/uz/oson/taskmanagementsystem/service/TaskImplService.java
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Service;
 import uz.oson.taskmanagementsystem.entity.Task;
 import uz.oson.taskmanagementsystem.entity.TaskStatus;
 import uz.oson.taskmanagementsystem.payload.TaskCreator;
+import uz.oson.taskmanagementsystem.payload.TaskResponse;
 import uz.oson.taskmanagementsystem.repository.TaskRepository;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -14,13 +18,29 @@ public class TaskImplService implements TaskService{
     private final TaskRepository taskRepository;
 
     @Override
-    public Task createTask(TaskCreator taskCreator) {
+    public TaskResponse createTask(TaskCreator taskCreator) {
         Task save = taskRepository.save(Task.builder()
                 .title(taskCreator.title())
                 .description(taskCreator.description())
                 .dueDate(taskCreator.dueDate())
                 .status(TaskStatus.OPEN)
                 .build());
-        return save;
+        return new TaskResponse(save.getId(), save.getTitle(), save.getDescription(), save.getDueDate(), save.getStatus());
+    }
+
+    @Override
+    public Set<TaskResponse> getAllTasks() {
+        Set<TaskResponse> taskResponses = new HashSet<>();
+        taskRepository.findAll().forEach(task -> {
+            taskResponses.add(new TaskResponse(
+                    task.getId(),
+                    task.getTitle(),
+                    task.getDescription(),
+                    task.getDueDate(),
+                    task.getStatus()
+            ));
+        });
+
+        return taskResponses;
     }
 }
