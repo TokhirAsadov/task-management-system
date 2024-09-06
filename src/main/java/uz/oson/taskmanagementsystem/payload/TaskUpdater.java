a878ddaa072ca0fc8decdfb3a3a3c95f88d80943
Takhir <guvalakat1603@gmail.com>
2024-09-06 18:58:32 +0500

    task updater record is created

---
+++ src/main/java/uz/oson/taskmanagementsystem/payload/TaskUpdater.java
@@ -0,0 +1,21 @@
+package uz.oson.taskmanagementsystem.payload;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import uz.oson.taskmanagementsystem.entity.TaskStatus;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+public record TaskUpdater(
+        @NotNull(message = "Id should be not null")
+        UUID id,
+        @NotNull(message = "Title should be not null")
+        @Size(min = 1, max = 255, message = "Title should have at least 2 characters")
+        String title,
+        String description,
+        Timestamp dueDate,
+        @NotNull(message = "Status should be not null; It can be once of OPEN, IN_PROGRESS, COMPLETED")
+        TaskStatus status
+        ) {
+}
