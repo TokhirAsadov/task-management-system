a0f03adf778cdcc1e91cca8c9b8ac6914e576ebf
Takhir <guvalakat1603@gmail.com>
2024-09-06 18:17:53 +0500

    getSingleTask function is created

--- src/main/java/uz/oson/taskmanagementsystem/service/TaskService.java
+++ src/main/java/uz/oson/taskmanagementsystem/service/TaskService.java
@@ -4,9 +4,12 @@ import uz.oson.taskmanagementsystem.payload.TaskCreator;
 import uz.oson.taskmanagementsystem.payload.TaskResponse;
 
 import java.util.Set;
+import java.util.UUID;
 
 public interface TaskService {
     TaskResponse createTask(TaskCreator taskCreator);
 
     Set<TaskResponse> getAllTasks();
+
+    TaskResponse getSingleTask(UUID id);
 }
