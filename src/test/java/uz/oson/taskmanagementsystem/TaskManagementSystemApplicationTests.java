3b65012db348d37e09501d0e15c0256f04224c74
Takhir <guvalakat1603@gmail.com>
2024-09-06 16:54:19 +0500

    Bismillah

---
+++ src/test/java/uz/oson/taskmanagementsystem/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.oson.taskmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
