a4570d434c8e6509bdbb68dd0ad3c5c95ae3828e
Takhir <guvalakat1603@gmail.com>
2024-09-06 18:18:03 +0500

    getSingleTask function is override and done

--- src/main/java/uz/oson/taskmanagementsystem/service/TaskImplService.java
+++ src/main/java/uz/oson/taskmanagementsystem/service/TaskImplService.java
@@ -4,12 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uz.oson.taskmanagementsystem.entity.Task;
 import uz.oson.taskmanagementsystem.entity.TaskStatus;
+import uz.oson.taskmanagementsystem.exceptions.DataNotFoundException;
 import uz.oson.taskmanagementsystem.payload.TaskCreator;
 import uz.oson.taskmanagementsystem.payload.TaskResponse;
 import uz.oson.taskmanagementsystem.repository.TaskRepository;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -42,5 +44,16 @@ public class TaskImplService implements TaskService{
         });
 
         return taskResponses;
+    }
+
+    @Override
+    public TaskResponse getSingleTask(UUID id) {
+        if (taskRepository.existsById(id)) {
+            Task task = taskRepository.findById(id).get();
+            return new TaskResponse(task.getId(), task.getTitle(), task.getDescription(), task.getDueDate(), task.getStatus());
+        }
+        else {
+            throw new DataNotFoundException("Task with id " + id + " not found");
+        }
     }
 }
