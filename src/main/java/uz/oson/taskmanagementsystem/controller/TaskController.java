6d5054a768c48adf2d5fff88b96c321bd42e986c
Takhir <guvalakat1603@gmail.com>
2024-09-06 18:13:09 +0500

    /tasks post api is created

--- src/main/java/uz/oson/taskmanagementsystem/controller/TaskController.java
+++ src/main/java/uz/oson/taskmanagementsystem/controller/TaskController.java
@@ -1,14 +1,34 @@
 package uz.oson.taskmanagementsystem.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import uz.oson.taskmanagementsystem.payload.TaskCreator;
+import uz.oson.taskmanagementsystem.payload.TaskResponse;
 import uz.oson.taskmanagementsystem.service.TaskService;
+
+import java.util.Set;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/tasks")
+@Slf4j
 public class TaskController {
 
     private final TaskService taskService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String taskCreating(@RequestBody @Valid TaskCreator creator){
+        log.info("Saving new task is starting, task creator is: {}",creator);
+        TaskResponse response = taskService.createTask(creator);
+        log.info("Saving new task is over, task id: {}", response.id());
+
+        return "task is created successfully";
+    }
+
+
+
 }
