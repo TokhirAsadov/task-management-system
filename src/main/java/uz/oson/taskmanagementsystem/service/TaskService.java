57f7c0e61045e9c7d0863553f10e8c516cdeae2e
Takhir <guvalakat1603@gmail.com>
2024-09-06 17:48:02 +0500

    createTask function is created

--- src/main/java/uz/oson/taskmanagementsystem/service/TaskService.java
+++ src/main/java/uz/oson/taskmanagementsystem/service/TaskService.java
@@ -1,4 +1,8 @@
 package uz.oson.taskmanagementsystem.service;
 
+import uz.oson.taskmanagementsystem.entity.Task;
+import uz.oson.taskmanagementsystem.payload.TaskCreator;
+
 public interface TaskService {
+    Task createTask(TaskCreator taskCreator);
 }
