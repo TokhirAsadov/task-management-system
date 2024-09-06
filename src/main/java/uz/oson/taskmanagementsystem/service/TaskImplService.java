b6b7ef508ea1b022c8784de854dec9b62846933d
Takhir <guvalakat1603@gmail.com>
2024-09-06 17:48:12 +0500

    createTask function is override and done

--- src/main/java/uz/oson/taskmanagementsystem/service/TaskImplService.java
+++ src/main/java/uz/oson/taskmanagementsystem/service/TaskImplService.java
@@ -2,6 +2,8 @@ package uz.oson.taskmanagementsystem.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uz.oson.taskmanagementsystem.entity.Task;
+import uz.oson.taskmanagementsystem.payload.TaskCreator;
 import uz.oson.taskmanagementsystem.repository.TaskRepository;
 
 @Service
@@ -9,4 +11,14 @@ import uz.oson.taskmanagementsystem.repository.TaskRepository;
 public class TaskImplService implements TaskService{
 
     private final TaskRepository taskRepository;
+
+    @Override
+    public Task createTask(TaskCreator taskCreator) {
+        Task save = taskRepository.save(Task.builder()
+                .title(taskCreator.title())
+                .description(taskCreator.description())
+                .dueDate(taskCreator.dueDate())
+                .build());
+        return save;
+    }
 }
