115fe911c740500a6854672e1a36d25d4580843a
Takhir <guvalakat1603@gmail.com>
2024-09-06 18:58:55 +0500

    updateTask function is created

--- src/main/java/uz/oson/taskmanagementsystem/service/TaskService.java
+++ src/main/java/uz/oson/taskmanagementsystem/service/TaskService.java
@@ -2,12 +2,14 @@ package uz.oson.taskmanagementsystem.service;
 
 import uz.oson.taskmanagementsystem.payload.TaskCreator;
 import uz.oson.taskmanagementsystem.payload.TaskResponse;
+import uz.oson.taskmanagementsystem.payload.TaskUpdater;
 
 import java.util.Set;
 import java.util.UUID;
 
 public interface TaskService {
     TaskResponse createTask(TaskCreator taskCreator);
+    TaskResponse updateTask(TaskUpdater taskUpdater);
 
     Set<TaskResponse> getAllTasks();
 
